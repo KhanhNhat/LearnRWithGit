4fd70dbe4b004b1aea4224c55eb5fb3d4281dcd6
KhanhNhat <hoangphamkhanhnhat@gmail.com>
2017-12-09 08:00:44 +1100

    Add a script for violin plot.

---
+++ ViolinPlot.R
@@ -0,0 +1,11 @@
+mammals2 = mammals %>%
+  group_by(vore) %>%
+  mutate(n = n()/nrow(mammals))
+
+#Just display how value is distributted
+ggplot(mammals, aes(x = vore, y = sleep_total, fill = vore)) +
+  geom_violin()
+
+#Absolute violin plot: how value is distributted and percent of each type of value in total dataset
+ggplot(mammals2, aes(x = vore, y = sleep_total, fill = vore)) +
+  geom_violin(aes(weight = TRUE), col = NA)
